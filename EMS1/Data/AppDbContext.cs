0812563fefdd04097f3d57562ed29133bc284bee
Parmesh Prasad Dash <159904466+parmesh1999@users.noreply.github.com>
2025-06-19 11:48:25 +0530

    Add project files.

---
+++ EMS1/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EMS1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+         public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EMS1/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EMS1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }       
+        public double Salary { get; set; }
+    }
+}
